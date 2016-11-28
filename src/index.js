39f7845dedb83165c2dc7008a0b1bcb1c9b42287
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2016-11-29 00:51:16 +0100

    Introduce sticky header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+window.$ = window.jQuery = require('jquery');
 import * as _ from 'lodash';
 import angular from 'angular';
-
 import './app/app.controllers';
 import './app/app.directives';
 
@@ -8,3 +8,17 @@ angular.module('cvApp', [
   'app.controllers',
   'app.directives',
 ]);
+
+$(document).ready(() => {
+  var appWrapper = $('#app-wrapper');
+
+  $(window).on('scroll', () => {
+    var scrollTop = $(window).scrollTop();
+
+    if (scrollTop > 15) {
+      appWrapper.addClass('fixed-header');
+    } else {
+      appWrapper.removeClass('fixed-header');
+    }
+  });
+});
